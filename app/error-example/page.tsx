1cf1a1890279739612679c0784416150d487b7bc
Anna Lopez <anna@komon.io>
2023-05-02 20:40:35 +0200

    third-session - wip - added error page examples

---
+++ app/error-example/page.tsx
@@ -0,0 +1,8 @@
+function ErrorExamplePage() {
+  //   const data = [0, 1];
+  //   const something = data[3].toFixed(2);
+  //   throw new Error("error example page");
+  return <div>ErrorExamplePage</div>;
+}
+
+export default ErrorExamplePage;
