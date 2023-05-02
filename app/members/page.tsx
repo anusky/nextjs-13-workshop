5bf6c0a8b55b463392d09adb0256160b8fac2ef8
Anna Lopez <anna@komon.io>
2023-05-02 20:39:42 +0200

    third-session - wip - added posts to show layouts, prefetching, loading,

---
+++ app/members/page.tsx
@@ -0,0 +1,13 @@
+import { MemberList } from "../ components/MemberList";
+
+function MembersPage() {
+  return (
+    <div>
+      MembersPage
+      {/* @ts-expect-error Server Component */}
+      <MemberList />
+    </div>
+  );
+}
+
+export default MembersPage;
